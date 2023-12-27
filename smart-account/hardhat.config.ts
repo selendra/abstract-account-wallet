32d85a035e7dcbd97a12760e91fefe2410ea83db
Nath242 <nath.selendra@gmail.com>
2023-12-27 10:50:13 +0700

    coomon type

--- smart-account/hardhat.config.ts
+++ smart-account/hardhat.config.ts
@@ -32,7 +32,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.19",
+        version: "0.8.17",
         settings: {
           optimizer: { enabled: true, runs: 800 },
           viaIR: true,
