72e43f654920201223087754a416c39431b55f28
Nath242 <nath.selendra@gmail.com>
2023-12-27 10:10:56 +0700

    init hardhat project

---
+++ smart-account/hardhat.config.ts
@@ -0,0 +1,62 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import * as dotenv from "dotenv";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
+import "@typechain/hardhat";
+
+const walletUtils = require("./walletUtils");
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const hardhatAccounts =
+  process.env.PRIVATE_KEY !== undefined
+    ? [process.env.PRIVATE_KEY]
+    : walletUtils.makeKeyList();
+
+const config: HardhatUserConfig = {
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    sources: "contracts",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: { enabled: true, runs: 800 },
+          viaIR: true,
+        },
+      },
+    ],
+  },
+  networks: {
+    selendra: {
+      url: process.env.SELENDRA_URL || "",
+      chainId: 1961,
+      accounts: hardhatAccounts,
+    },
+    testnet: {
+      url: process.env.TESTNET_URL || "",
+      chainId: 1953,
+      accounts: hardhatAccounts,
+    },
+  },
+
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    onlyCalledMethods: true,
+  },
+};
+
+export default config;
---
+++ smart-account/walletUtils.js
@@ -0,0 +1,75 @@
+// eslint-disable-next-line node/no-unpublished-require
+const ethers = require("ethers");
+
+const fs = require("fs");
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+const makeKeyList = (
+  num = 5,
+  mn = mnemonic,
+  index = 0,
+  path = "m/44'/60'/0'/0/"
+) => {
+  const accounts = [];
+  let i;
+  for (i = 0; i < num; i++) {
+    const mnemonicInstance = ethers.Mnemonic.fromPhrase(mn);
+    accounts.push(ethers.HDNodeWallet.fromMnemonic(mnemonicInstance, path + i).privateKey);
+  }
+  return accounts;
+};
+
+const makeSignerList = (
+  num = 1,
+  mn = mnemonic,
+  index = 0,
+  path = "m/44'/60'/0'/0/"
+) => {
+  const accounts = [];
+  let i;
+  for (i = 0; i < num; i++) {
+    const mnemonicInstance = ethers.Mnemonic.fromPhrase(mn);
+    accounts.push(ethers.HDNodeWallet.fromMnemonic(mnemonicInstance, path + i));
+  }
+  return accounts;
+};
+
+const localWallet = (
+  b,
+  num = 1,
+  mn = mnemonic,
+  index = 0,
+  path = "m/44'/60'/0'/0/"
+) => {
+  const hdW = makeKeyList(num, mn, index, path);
+  const lW = [];
+  let i;
+  for (i = 0; i < hdW.length; i++) {
+    lW.push({ privateKey: hdW[i], balance: b });
+  }
+  return lW;
+};
+
+const ganacheWallet = (
+  b,
+  num = 1,
+  mn = mnemonic,
+  index = 0,
+  path = "m/44'/60'/0'/0/"
+) => {
+  const hdW = makeKeyList(num, mn, index, path);
+  const lW = [];
+  let i;
+  for (i = 0; i < hdW.length; i++) {
+    lW.push({ secretKey: hdW[i], balance: b });
+  }
+  return lW;
+};
+
+const walletUtils = () => {};
+walletUtils.makeKeyList = makeKeyList;
+walletUtils.makeSignerList = makeSignerList;
+walletUtils.localWallet = localWallet;
+walletUtils.ganacheWallet = ganacheWallet;
+
+module.exports = walletUtils;
