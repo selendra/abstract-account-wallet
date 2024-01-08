a258b9d74c6a1fce3c6d4a2333799818af1d75ba
Nath242 <nath.selendra@gmail.com>
2024-01-08 07:35:36 +0700

    deploy script

--- smart-account/scripts/deploy.ts
+++ smart-account/scripts/deploy.ts
@@ -1,27 +1,97 @@
-import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+import { ethers, run } from "hardhat";
+import { formatEther } from "ethers/lib/utils";
 
-  const lockedAmount = ethers.parseEther("0.001");
+const smartAccountFactoryOwnerAddress =
+  process.env[`SMART_ACCOUNT_FACTORY_OWNER_ADDRESS}`]!;
+const paymasterOwnerAddress =
+  process.env[`PAYMASTER_OWNER_ADDRESS`]!;
+const verifyingSigner =
+  process.env[`PAYMASTER_SIGNER_ADDRESS`]!;
+const DEPLOYER_CONTRACT_ADDRESS =
+  process.env[`DEPLOYER_CONTRACT_ADDRESS`]!;
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+const contractsDeployed: Record<string, string> = {};
 
-  await lock.waitForDeployment();
+export async function deployGeneric(
+  contractName: string,
+  constructorArguments: any
+) {
+  const contractFactory = await ethers.getContractFactory(contractName);
+  const contract = await contractFactory.deploy(constructorArguments);
+  await contract.deployed();
+  contractsDeployed[contractName] = contract.address;
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `${contractName} is deployed with address ${contract.address}`
+  );
+
+  return contract.address;
+}
+
+async function deployEntryPointContract() {
+  await deployGeneric(
+    "EntryPoint",
+    []
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function deployBaseWalletImpContract(entryPointAddress: string){
+  await deployGeneric(
+    "SmartAccount",
+    entryPointAddress
+  );
+}
+
+export async function mainDeploy(): Promise<Record<string, string>> {
+  // verifyDeploymentConfig();
+
+  console.log("=========================================");
+  console.log(
+    "Smart Account Factory Owner Address: ",
+    smartAccountFactoryOwnerAddress
+  );
+  console.log("Paymaster Owner Address: ", paymasterOwnerAddress);
+  console.log("Verifying Signer Address: ", verifyingSigner);
+  console.log("Deployer Contract Address: ", DEPLOYER_CONTRACT_ADDRESS);
+
+  const [deployer] = await ethers.getSigners();
+  const deployerBalanceBefore = await deployer.getBalance();
+  console.log(
+    `Deployer ${deployer.address} initial balance: ${formatEther(
+      deployerBalanceBefore
+    )}`
+  );
+  console.log("=========================================");
+
+  await deployEntryPointContract();
+  await deployBaseWalletImpContract(contractsDeployed["EntryPoint"])
+  console.log("=========================================");
+
+  console.log(
+    "Deployed Contracts: ",
+    JSON.stringify(contractsDeployed, null, 2)
+  );
+  console.log("=========================================");
+
+  const deployerBalanceAfter = await deployer.getBalance();
+  console.log(
+    `Deployer ${deployer.address} final balance: ${formatEther(
+      deployerBalanceAfter
+    )}`
+  );
+  console.log(
+    `Funds used: ${formatEther(
+      deployerBalanceBefore.sub(deployerBalanceAfter)
+    )}`
+  );
+
+  return contractsDeployed;
+}
+
+if (require.main === module) {
+  mainDeploy().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+}
