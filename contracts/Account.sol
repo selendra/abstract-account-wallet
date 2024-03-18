499d68ab42bbf026d8c270eca4a5b1b5e58ac174
Nath242 <nath.selendra@gmail.com>
2024-03-18 12:22:43 +0700

    test step

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -2,7 +2,25 @@
 pragma solidity ^0.8.19;
 
 import "@account-abstraction/contracts/core/EntryPoint.sol";
+import "@account-abstraction/contracts/interfaces/IAccount.sol";
 
-contract Account {
-   
+contract Account is IAccount {
+    uint256 public count;
+    address public owner;
+
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        pure
+        returns (uint256 validationData)
+    {
+        return 0;
+    }
+
+    function execute() external {
+        count++;
+    }
 }
